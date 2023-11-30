ed0d97da38de12f237731d18163001f34f70fa65
masterSkill <clairmont.rajaonarison@gmail.com>
2023-11-30 17:30:41 +0300

    fix : prefix API and auth for login

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -13,28 +13,27 @@ class LoginTest extends TestCase
      */
     public function test_login_page(): void
     {
-        $response = $this->get('/login');
+        $response = $this->get('/api/auth/login');
 
         $response->assertStatus(200);
     }
 
-    public function test_log_user_failed() : void
+    public function test_log_user_failed(): void
     {
-        $response = $this->post('/login', []);
+        $response = $this->post('/api/auth/login', []);
         $response->assertBadRequest();
     }
 
-    public function test_log_user_failed() : void
+    public function test_log_user_password_email_failed(): void
     {
-        $response = $this->post('/login', ['email' => 'jhon.doe@gmail.com', 'password' => '123456789']);
+        $response = $this->post('/api/auth/login', ['email' => 'jhon.doe@gmail.com', 'password' => '123456789']);
         $response->assertUnauthorized();
     }
 
-
-    public function test_log_user_ok() : void
+    public function test_log_user_ok(): void
     {
-        $response = $this->post('/login', ['email' => 'koders@koders.mg', 'password' => '123456789']);
-        $response->assertAccepted();
+        $response = $this->post('/api/auth/login', ['email' => 'koders@koders.mg', 'password' => '123456789']);
+        $response->assertOk();
         $response->assertJsonStructure(['user', 'token']);
     }
 }
