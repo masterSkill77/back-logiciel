90997b5298cb51a54a5bcb2a9415475cc1c5d4ce
masterSkill <clairmont.rajaonarison@gmail.com>
2023-12-12 14:04:27 +0300

    feat : update doc for the scopeAgency fuction

--- app/Models/Agency.php
+++ app/Models/Agency.php
@@ -13,9 +13,12 @@ class Agency extends Model
     protected $fillable = ['nameAgency', 'nameCompany', 'addressCompany', 'phoneAgency'];
 
     /**
-     * Scope for specifing directly the agenceId without making the condition `where('id', agenceId)
+     * Scope for specifing directly the agenceId without making the condition `where('id', agenceId)`
+     * @param Illuminate\Database\Eloquent\Builder $builder
+     * @param int $agenceId
+     * @return Illuminate\Database\Eloquent\Builder
      */
-    public function scopeAgence(Builder $builder, int $agenceId): Builder
+    public function scopeAgency(Builder $builder, int $agenceId): Builder
     {
         return $builder->where('id', $agenceId);
     }
