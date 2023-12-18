750910a946e3a325fbfee7ee4a638c3bc1213197
masterSkill <clairmont.rajaonarison@gmail.com>
2023-12-18 09:12:48 +0300

    feat : update the relationship between user and agency

--- app/Models/Agency.php
+++ app/Models/Agency.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Agency extends Model
 {
@@ -21,5 +22,10 @@ class Agency extends Model
     public function scopeAgency(Builder $builder, int $agenceId): Builder
     {
         return $builder->where('id', $agenceId);
+    }
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class, 'agence_id');
     }
 }
