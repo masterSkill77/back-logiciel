7ce79d74645aaffc176024b84e82730e619c6a6a
Tojo Randrianantenaina <tojonantenaina08@gmail.com>
2024-01-09 20:45:39 +0300

    feat: migration and model Diagnostic

---
+++ app/Models/Diagnostic.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Diagnostic extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'id_diagnostics';
+
+    protected $fillable = ['year_construction', 'dpe_date_realization', 'dpe_property', 'dpe_virgin', 'dpe_consommation', 'dpe_ges', 'cost_estimate', 'ref_year'
+    , 'amiante', 'amiante_yes_date', 'amiante_yes_comments', 'electric', 'electric_yes_date', 'electric_yes_date', 'electric_yes_comments', 'gaz', 'gaz_yes_date', 'gaz_yes_comments'
+    , 'plomb', 'plomb_yes_date', 'plomb_yes_comments', 'loi_carrez', 'loi_carrez_yes_date', 'loi_carrez_yes_comments', 'erp', 'erp_yes_date', 'erp_yes_comments'
+    , 'state_parasitaire', 'state_parasitaire_yes_date', 'state_parasitaire_yes_comments', 'assainissement', 'assainissement_yes_date', 'assainissement_yes_comments'];
+
+
+}
---
+++ database/migrations/2024_01_07_225804_create_diagnostics_table.php
@@ -0,0 +1,93 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('diagnostics', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_diagnostics');
+            //ANNÉE DE CONSTRUCTION:
+            $table->integer('year_construction')->nullable();
+            //DPE_DATE DE RÉALISATION
+            $table->date('dpe_date_realization')->nullable();
+            //DPE_BIEN SOUMIS AU DPE:
+            $table->boolean('dpe_property')->default(false);
+            //DPE VIERGE
+            $table->boolean('dpe_virgin')->default(false);
+            //DPE_CONSOMMATION
+            $table->string('dpe_consommation')->nullable();
+            //GES
+            $table->string('dpe_ges')->nullable();
+            //ESTIMATION COÛTS ANNUELS MOYENS (€):
+            $table->integer('cost_estimate')->nullable();
+            //ANNÉE DE RÉFÉRENCE: 
+            $table->integer('ref_year')->nullable();
+            //AMIANTE
+            $table->integer('amiante')->nullable();
+            //AMIANTE SI oui DATE
+            $table->date('amiante_yes_date')->nullable();
+            // AMIANTE SI oui COMMENTAIRE
+            $table->string('amiante_yes_comments')->nullable();
+            // ÉLECTRIQUE
+            $table->integer('electric')->nullable();
+            // ELECTRIQUE Si oui DATE
+            $table->integer('electric_yes_date')->nullable();
+            // ELECTRIQUE SI oui COMMENTAIRE 
+            $table->string('electric_yes_comments')->nullable();
+            // GAZ
+            $table->integer('gaz')->nullable();
+            // GAZ Si oui DATE
+            $table->integer('gaz_yes_date')->nullable();
+            // GAZ SI oui COMMENTAIRE 
+            $table->string('gaz_yes_comments')->nullable();
+             // PLOMB
+             $table->integer('plomb')->nullable();
+             // PLOMB Si oui DATE
+             $table->integer('plomb_yes_date')->nullable();
+             // PLOMB SI oui COMMENTAIRE 
+             $table->string('plomb_yes_comments')->nullable();
+            // LOI CARREZ
+            $table->integer('loi_carrez')->nullable();
+            // LOI CARREZ Si oui DATE
+            $table->integer('loi_carrez_yes_date')->nullable();
+            // LOI CARREZ SI oui COMMENTAIRE 
+            $table->string('loi_carrez_yes_comments')->nullable();
+            // ERP
+            $table->integer('erp')->nullable();
+            // ERP Si oui DATE
+            $table->integer('erp_yes_date')->nullable();
+            // ERP SI oui COMMENTAIRE 
+            $table->string('erp_yes_comments')->nullable();
+            // ÉTAT PARASITAIRE
+            $table->integer('state_parasitaire')->nullable();
+            // ÉTAT PARASITAIRE Si oui DATE
+            $table->integer('state_parasitaire_yes_date')->nullable();
+            // ÉTAT PARASITAIRE SI oui COMMENTAIRE 
+            $table->string('state_parasitaire_yes_comments')->nullable();
+             //ASSAINISSEMENT
+            $table->integer('assainissement')->nullable();
+            // ASSAINISSEMENT Si oui DATE
+            $table->integer('assainissement_yes_date')->nullable();
+            // ASSAINISSEMENT SI oui COMMENTAIRE 
+            $table->string('assainissement_yes_comments')->nullable();    
+            
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('diagnostics');
+    }
+};
