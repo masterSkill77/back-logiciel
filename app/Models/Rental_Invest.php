bd913e2884c82030ac8cb25b4368d75d1e81dd7c
Tojo Randrianantenaina <tojonantenaina08@gmail.com>
2024-01-09 21:35:20 +0300

    feat: migration and model Investissement Locatif

---
+++ app/Models/Rental_Invest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rental_Invest extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey  = 'id_rental_invest';
+
+    protected $fillable = ['estimated_rent', 'monthly_rent', 'yeld', 'occupancy'];
+}
