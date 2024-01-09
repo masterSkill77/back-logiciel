53dfc505059164e0963350ab01dd78cff9b2abef
Tojo Randrianantenaina <tojonantenaina08@gmail.com>
2024-01-09 22:13:54 +0300

    feat: model and migration Photos

---
+++ app/Models/Photos.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Photos extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'id_photos';
+
+
+    protected $fillable = ['photos_original', 'photos_slide'];
+
+    protected $casts = [
+        'photos_slide' => 'json'
+    ];
+}
