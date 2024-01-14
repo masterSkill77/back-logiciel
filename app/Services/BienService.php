e63e5bfb3a3c9bc82c9ca4b70e81aa72adf2fead
Fabriola Hightao <fabriola@hightao-mg.com>
2024-01-14 09:18:24 +0300

    service bien

--- app/Services/BienService.php
+++ app/Services/BienService.php
@@ -2,10 +2,36 @@
 
 namespace App\Services;
 
+use App\Models\Bien;
+use Illuminate\Database\Eloquent\Collection;
+
 class BienService
 {
     public function __construct()
     {
         // Constructor of the class
+    }
+
+    public function createBien(array $params): Bien
+    {
+        $Bien = (new Bien($params));
+        $Bien->save();
+        return $Bien;
+    }
+
+    public function updateBien(Bien $bien, array $params): Bien
+    {
+        $bien->update($params);
+        return $bien;
+    }
+
+    public function getBien(): Collection
+    {
+        return Bien::all();
+    }
+
+    public function getById(int $bienId): Bien
+    {
+        return Bien::where('id', $bienId)->first();
     }
 }
