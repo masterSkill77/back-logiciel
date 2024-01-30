7341996c5d0e88c53c70b47bd669ddf388552bd5
masterSkill <clairmont.rajaonarison@gmail.com>
2024-01-30 15:30:48 +0300

    feat ; protect all routes

--- routes/api.php
+++ routes/api.php
@@ -36,51 +36,47 @@ Route::prefix("auth")->group(function () {
     Route::post("login", [LoginController::class, "login"]);
 });
 
-Route::prefix("user")->middleware(["auth:sanctum"])->group(function () {
-    Route::post("/create", [UserController::class, "store"]);
-    Route::get('/get-agents', [UserController::class, "getAllAgents"]);
-});
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix("user")->middleware(["auth:sanctum"])->group(function () {
+        Route::post("/create", [UserController::class, "store"]);
+        Route::get('/get-agents', [UserController::class, "getAllAgents"]);
+    });
 
-Route::prefix("contact")->middleware(["auth:sanctum"])->group(function () {
-    Route::post('/', [ContactController::class, 'store']);
-    Route::get('/', [ContactController::class, 'index']);
-});
+    Route::prefix("contact")->middleware(["auth:sanctum"])->group(function () {
+        Route::post('/', [ContactController::class, 'store']);
+        Route::get('/', [ContactController::class, 'index']);
+    });
 
 
-// route pour les type offre
-Route::prefix("type-offert")->group(function () {
-    Route::get('/', [TypeOffertController::class, 'index']);
-    Route::get('/{id}', [TypeOffertController::class, 'findById']);
-    Route::get('/classification-offert/{id}', [TypeOffertController::class, 'getClassificationByIdTypeOffert']);
-});
+    // route pour les type offre
+    Route::prefix("type-offert")->group(function () {
+        Route::get('/', [TypeOffertController::class, 'index']);
+        Route::get('/{id}', [TypeOffertController::class, 'findById']);
+        Route::get('/classification-offert/{id}', [TypeOffertController::class, 'getClassificationByIdTypeOffert']);
+    });
 
-// route pour les type de bien
-Route::prefix("type-estate")->group(function () {
-    Route::get('/', [TypeEstateController::class, 'index']);
-    Route::get('/{id}', [TypeEstateController::class, 'getById']);
-    Route::get('/classification-bien/{id}', [TypeEstateController::class, 'getClassificationByIdTypeEstate']);
-});
+    // route pour les type de bien
+    Route::prefix("type-estate")->group(function () {
+        Route::get('/', [TypeEstateController::class, 'index']);
+        Route::get('/{id}', [TypeEstateController::class, 'getById']);
+        Route::get('/classification-bien/{id}', [TypeEstateController::class, 'getClassificationByIdTypeEstate']);
+    });
 
-// route pour les classification de bien
-Route::prefix("classification-estate")->group(function () {
-    Route::get('/', [ClassificationEstateController::class, 'index']);
-    Route::get('/{id}', [ClassificationEstateController::class, 'getById']);
-});
+    // route pour les classification de bien
+    Route::prefix("classification-estate")->group(function () {
+        Route::get('/', [ClassificationEstateController::class, 'index']);
+        Route::get('/{id}', [ClassificationEstateController::class, 'getById']);
+    });
 
-// route pour les biens
-Route::prefix("bien")->group(function () {
-    Route::post('/', [BienController::class, 'createBien']);
-    Route::get('/', [BienController::class, 'findAll']);
-});
-
-// route pour les biens
-Route::prefix("bien")->group(function () {
-    Route::post('/', [BienController::class, 'createBien'])->middleware(['auth:sanctum']);
-    Route::get('/', [BienController::class, 'findAll'])->middleware(['auth:sanctum']);
-    Route::get('/{id}', [BienController::class, 'findById']);
-    Route::post('/photos', [BienController::class, 'testPhotos']);
-    Route::prefix('agency')->group(function () {
-        Route::get('/', [AgencyController::class, 'show'])->middleware(["role:" . (Role::SUPER_ADMIN)->value]);
+    // route pour les biens
+    Route::prefix("bien")->group(function () {
+        Route::post('/', [BienController::class, 'createBien'])->middleware(['auth:sanctum']);
+        Route::get('/', [BienController::class, 'findAll'])->middleware(['auth:sanctum']);
+        Route::get('/{id}', [BienController::class, 'findById']);
+        Route::post('/photos', [BienController::class, 'testPhotos']);
+        Route::prefix('agency')->group(function () {
+            Route::get('/', [AgencyController::class, 'show'])->middleware(["role:" . (Role::SUPER_ADMIN)->value]);
+        });
     });
     Route::prefix('piges')->group(function () {
         Route::get('/{agency}', [PigeController::class, 'getPigesByAgence']);
@@ -88,6 +84,5 @@ Route::prefix("bien")->group(function () {
         Route::post('/postal-code', [PigeController::class, 'createPostalCode'])->middleware(["role:" . (Role::SUPER_ADMIN)->value]);
     });
 });
-
 
 Route::get('/validate-account', ConfirmationAccountController::class)->name('validate.account');
