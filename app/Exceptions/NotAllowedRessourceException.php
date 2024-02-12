1cfa62ea1493c838dda01ae0c5bec8dc2b6d41fc
masterSkill <clairmont.rajaonarison@gmail.com>
2024-02-12 15:53:43 +0300

    feat : update the not allowed resource exception

--- app/Exceptions/NotAllowedRessourceException.php
+++ app/Exceptions/NotAllowedRessourceException.php
@@ -8,6 +8,6 @@ class NotAllowedRessourceException extends Exception
 {
     public function report()
     {
-        return response()->json(['error' => 'NOT_ALLOWED_RESSOURCE', 'message' => "Vous n'avez les autorisations suffisantes pour accéder à ce ressource"], 403);
+        return response()->json(['error' => 'NOT_ALLOWED_RESSOURCE', 'message' => "Vous n'avez les autorisations suffisantes pour accéder à ce ressource, la ressource étant pour une autre agence que la votre"], 403);
     }
 }
