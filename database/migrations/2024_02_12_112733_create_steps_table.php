3b4fca5ef6160e5d39cd2bfc3b8506ac6bfcfec8
masterSkill <clairmont.rajaonarison@gmail.com>
2024-02-12 14:35:08 +0300

    fix : integer instead of number

--- database/migrations/2024_02_12_112733_create_steps_table.php
+++ database/migrations/2024_02_12_112733_create_steps_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('steps', function (Blueprint $table) {
             $table->id();
             $table->string('step_name');
-            $table->number('step_duration');
+            $table->integer('step_duration');
             $table->date('step_date');
             $table->foreignIdFor(Folder::class);
             $table->timestamps();
