c00ae279bb470ed7fc0bf8a3000d45e432fc2113
masterSkill <clairmont.rajaonarison@gmail.com>
2024-03-01 16:14:38 +0300

    feat : add filter

--- app/Filters/EstimationFilters.php
+++ app/Filters/EstimationFilters.php
@@ -30,8 +30,44 @@ class EstimationFilters extends QueryFilters
     }
 
 
-    // public function nb_pieces(string $nbPieces = '')
-    // {
-    //     return $nbPieces == '' ? $this->builder : $this->builder->where('bien', $typeBien);
-    // }
+    public function nb_pieces(string $nbPieces = '')
+    {
+        if ($nbPieces == '') {
+            return $this->builder;
+        }
+        return $this->builder->whereRaw("JSON_EXTRACT(details_bien, '$.nb_pieces') = ?", [$nbPieces]);
+    }
+
+    public function nb_chambre(string $nbChambre = '')
+    {
+        if ($nbChambre == '') {
+            return $this->builder;
+        }
+        return $this->builder->whereRaw("JSON_EXTRACT(details_bien, '$.nb_chambre') = ?", [$nbChambre]);
+    }
+
+    public function nb_salle_eau(string $nbSalleEau = '')
+    {
+        if ($nbSalleEau == '') {
+            return $this->builder;
+        }
+        return $this->builder->whereRaw("JSON_EXTRACT(details_bien, '$.nb_salle_eau') = ?", [$nbSalleEau]);
+    }
+
+
+    public function nb_salle_bain(string $nbSalleBain = '')
+    {
+        if ($nbSalleBain == '') {
+            return $this->builder;
+        }
+        return $this->builder->whereRaw("JSON_EXTRACT(details_bien, '$.nb_salle_bain') = ?", [$nbSalleBain]);
+    }
+
+    public function surface_habitable(string $surfaceHabitable = '')
+    {
+        if ($surfaceHabitable == '') {
+            return $this->builder;
+        }
+        return $this->builder->whereRaw("JSON_EXTRACT(details_bien, '$.surface_habitable') = ?", [$surfaceHabitable]);
+    }
 }
