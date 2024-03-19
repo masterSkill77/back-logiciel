49749b76c3296b4a80c5ddadf39a520d9ffa72a5
Osmin <osm1.zandry@gmail.com>
2024-03-19 22:06:25 +0100

    update: modification api chatgpt

--- app/Http/Controllers/API/ChatGPTController.php
+++ app/Http/Controllers/API/ChatGPTController.php
@@ -5,17 +5,22 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Annonce\AnnonceRequest;
 use App\Services\OpenAiService;
+use App\Services\TypeEstateService;
 use Orhanerday\OpenAi\OpenAi;
 
 class ChatGPTController extends Controller
 {
-    public function __construct(public OpenAiService $openAiService)
+    public function __construct(public OpenAiService $openAiService, public TypeEstateService $typeEstateService)
     {
         
     }
-    public function chat(AnnonceRequest $annonceRequest)
+    public function chatGPT(AnnonceRequest $annonceRequest)
     {
+        var_dump($annonceRequest);die;
         $annonces = $annonceRequest->toArray();
+        $offert = $this->typeEstateService->getById($annonces['type_offert_id']);
+        var_dump($offert);
+        die;
         $chat = $this->openAiService->useOpenAi($annonces);
         $data = json_decode($chat, true);
 
