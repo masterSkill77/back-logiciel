3357f1ee6e85806a97737c1fef880df6cb577d87
masterSkill <clairmont.rajaonarison@gmail.com>
2024-03-27 11:13:08 +0300

    fix : no index biens on watchFields from the formBuilder

--- app/Services/OpenAiService.php
+++ app/Services/OpenAiService.php
@@ -18,9 +18,9 @@ class OpenAiService
                     "role" => "user",
                     "content" => "Je veux une description d'annonce basée sur les critères suivantes:
                                     Une " . $offert . " de " . $estate .
-                                    "dont le prix est de " . $annonces['biens']['publish_price'] .
-                                    "euro, sa surface habitable est de " . $annonces['biens']['living_area'] ."m².
-                                    J'aimerais que cette offre soit dans la ville de " . $annonces['biens']['city'] . ".Avec ou sans jardin,
+                                    "dont le prix est de " . $annonces['publish_price'] .
+                                    "euro, sa surface habitable est de " . $annonces['living_area'] ."m².
+                                    J'aimerais que cette offre soit dans la ville de " . $annonces['city'] . ".Avec ou sans jardin,
                                     avoir de la piscine, possédant du garage ou parking. Je veux aussi savoir le nombre de salle de bains,
                                     les équipements cuisinières. Une". $estate . "avec de la terasse. Son mode de chauffage en indiquant le format et le type d'énergie utilisé.
                                     S'il y a de climatisation aussi et des volets electriques."
